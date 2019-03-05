91a5b991aba99f66508e1b77562039b9c10ab96a
frokenfreyja <freyjasigur@gmail.com>
2019-03-05 21:29:14 +0000

    .

--- todos.js
+++ todos.js
@@ -21,28 +21,33 @@ function isEmpty(s) {
  *
  * @returns {array} Array of validation of errors, empty error if no errors
  */
-function validateNote({ title, position, completed, due }) {
+function validateTodo({ title, position, completed, due }) {
   const errors = [];
 
-  if (typeof title !== 'string' || !validator.isLength(title, { min: 1, max: 128 })) {
-    errors.push({
-      field: 'title',
-      message: 'Titill verður að vera strengur sem er 1 til 128 stafir',
-    });
+  if (!isEmpty(title)) {
+    if (typeof title !== 'string' || !validator.isLength(title, { min: 1, max: 128 })) {
+      errors.push({
+        field: 'title',
+        message: 'Titill verður að vera strengur sem er 1 til 128 stafir',
+      });
+    }
   }
 
-  if (typeof position !== 'number' || position < 0) {
-    errors.push({
-      field: 'position',
-      message: 'Staðsetning verður að vera heiltala stærri eða jöfn 0',
-    });
+  if (!isEmpty(position)) {
+    if (typeof position !== 'number' || position < 0) {
+      errors.push({
+        field: 'position',
+        message: 'Staðsetning verður að vera heiltala stærri eða jöfn 0',
+      });
+    }
   }
-
-  if (typeof completed !== 'boolean') {
-    errors.push({
-      field: 'completed',
-      message: 'Lokið verður að vera boolean gildi',
-    });
+  if (!isEmpty(completed)) {
+    if (typeof completed !== 'boolean') {
+      errors.push({
+        field: 'completed',
+        message: 'Lokið verður að vera boolean gildi',
+      });
+    }
   }
   
   if (!isEmpty(due)) {
@@ -97,7 +102,7 @@ async function query(sqlQuery, values = []) {
  * @returns {Promise} Promise representing the object result of creating the todo
  */
 async function create({ title, position, completed, due} = {}) {
-  const validation = validateNote({ title, position, completed, due });
+  const validation = validateTodo({ title, position, completed, due });
 
   if (validation.length > 0) {
     return {
@@ -170,8 +175,15 @@ async function readOne(id) {
  *
  * @returns {Promise} Promise representing the object result of creating the todo
  */
+
+/*
 async function update(id, { title, due, position, completed } = {}) {
   const validation = validateNote({ title, due, position, completed });
+
+  if(!validation) {
+    console.log('hallo', validation);
+  } else {
+  }
 
   if (validation.length > 0) {
     return {
@@ -179,7 +191,6 @@ async function update(id, { title, due, position, completed } = {}) {
       validation,
     };
   }
-
   const cleanTitle = xss(title);
   const cleanDue = xss(due);
   const cleanPosition = xss(position);
@@ -202,6 +213,67 @@ async function update(id, { title, due, position, completed } = {}) {
     success: true,
     validation: [],
     item: result.rows[0],
+  };
+}
+*/
+
+
+async function update(id, { title, position, completed, due } = {}) {
+  const result = await query('SELECT * FROM todos where id = $1', [id]);
+
+  if (result.rows.length === 0) {
+    return {
+      success: false,
+      notFound: true,
+      validation: [],
+    };
+  }
+
+  const validationResult = await validateTodo({ title, due, position, completed });
+
+  if (validationResult.length > 0) {
+    return {
+      success: false,
+      notFound: false,
+      validation: validationResult,
+    };
+  }
+
+  const changedColumns = [
+    !isEmpty(title) ? 'title' : null,
+    !isEmpty(due) ? 'due' : null,
+    !isEmpty(position) ? 'position' : null,
+    !isEmpty(completed) ? 'completed' : null,
+  ].filter(Boolean);
+
+  const changedValues = [
+    !isEmpty(title) ? xss(title) : null,
+    !isEmpty(due) ? xss(due) : null,
+    !isEmpty(position) ? xss(position) : null,
+    !isEmpty(completed) ? xss(completed) : null,
+  ].filter(Boolean);
+
+  const updates = [id, ...changedValues];
+
+  const updatedColumnsQuery =
+    changedColumns
+      .map((column, i) => `${column} = $${i + 2}`);
+
+  console.log(updates);
+  console.log(updatedColumnsQuery);
+
+  const q = `
+    UPDATE todos
+    SET ${updatedColumnsQuery.join(', ')}
+    WHERE id = $1
+    RETURNING id, title, due, position, completed`;
+  console.log(q);
+
+  const updateResult = await query(q, updates);
+  console.log(updateResult);
+  return {
+    success: true,
+    item: updateResult.rows[0],
   };
 }
 
